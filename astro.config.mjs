81522ab42ed67cc4fdfbb94cbe40463582c8925b
edwardqs <ventas1@pauserdistribucionessac.onmicrosoft.com>
2026-03-05 11:25:02 -0500

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,14 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
     output: "server",
-    adapter: node({ mode: "standalone" }),
+    adapter: node({ 
+        mode: "standalone" 
+    }),
+    // Añadimos esta sección para configurar el puerto y el host
+    server: {
+        port: 3000,
+        host: true // Esto habilita el host 0.0.0.0
+    },
     integrations: [react()],
     vite: {
         plugins: [tailwindcss()],
